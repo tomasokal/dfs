215ce292911e080611cf536933be4e48acfcfc95
Tomas Okal <tomas.okal@gmail.com>
2020-09-11 15:24:54 -0500

    Salaries done.

--- dfs_projections.R
+++ dfs_projections.R
@@ -4,6 +4,8 @@ library(rvest)
 
 # Generate list of webpages to scrape
 
+week_number <- 1
+
 web_page_generate <- function(position, week) {
   
   web_page <- paste0("https://www.fantasypros.com/nfl/projections/"
@@ -17,7 +19,7 @@ web_page_generate <- function(position, week) {
 
 position_list <- list(c("qb", "rb", "wr", "te", "k", "dst"))
 
-web_pages_list <- lapply(position_list, web_page_generate, week = 1)
+web_pages_list <- lapply(position_list, web_page_generate, week = week_number)
 web_pages_list <- as.list(unlist(web_pages_list))
 
 # Generate list of scraped tables
@@ -86,6 +88,8 @@ projection_table_list2 <- mapply(cbind, projection_table_list1, "POSITION" = pos
 
 projection_table_combined <- data.table::rbindlist(projection_table_list2)
 
+# Remove white space and extract team name
+
 LEFT = function(x,n){
   substring(x,1,nchar(x)-n)
 }
@@ -94,54 +98,12 @@ RIGHT = function(x,n){
   substring(x,nchar(x)-n+1)
 }
 
-DST = function(x) {
-  
-  x <- switch(x
-         , "Buffalo Bills" = "BUF"
-         , "New England Patriots" = "NE"
-         , "Pittsburgh Steelers" = "PIT"
-         , "Philadelphia Eagles" = "PHI"
-         , "Baltimore Ravens" = "BAL"
-         , "Indianpolis Colts" = "IND"
-         , "San Francisco 49ers" = "SF"
-         , "Kansas City Chiefs" = "KC"
-         , "Denver Broncos" = "DEN"
-         , "Los Angeles Chargers" = "LAC"
-         , "Cincinnati Bengals" = "CIN"
-         , "Tennessee Titans" = "TEN"
-         , "Las Vegas Raiders" = "LV"
-         , "New York Jets" = "NYJ"
-         , "Arizona Cardinals" = "ARI"
-         , "Atlanta Falcons" = "ATL"
-         , "Green Bay Packers" = "GB"
-         , "Detroit Lions" = "DET"
-         , "Los Angeles Rams" = "LAR"
-         , "Miami Dolphins" = "MIA"
-         , "Chicago Bears" = "CHI"
-         , "Dallas Cowboys" = "DAL"
-         , "Seattle Seahawks" = "SEA"
-         , "Washington Football Team" = "WAS"
-         , "Caroline Panthers" = "CAR"
-         , "Cleveland Browns" = "CLE"
-         , "Minnesota Vikings" = "MIN"
-         , "New Orleans Saints" = "NO"
-         , "Jacksonville Jaguars" = "JAC"
-         , "Tampa Bay Buccaneers" = "TB"
-         , "New York Giants" = "NYG"
-         , "Houston Texans" = "HOU")
-  
-  return(class(x))
-  
-}
-
-DST("Buffalo Bills")
-
 final_projections <- projection_table_combined[
   
   ,
   j = .(POSITION
         , PLAYER_NAME = ifelse(POSITION == "DST", PLAYER.V1, trimws(LEFT(PLAYER.V1, 3)))
-        , TEAM = ifelse(POSITION == "DST", sapply(PLAYER.V1
+        , TEAM1 = ifelse(POSITION == "DST", sapply(PLAYER.V1
                                                   , switch
                                                   , "Buffalo Bills" = "BUF"
                                                   , "New England Patriots" = "NE"
@@ -175,6 +137,15 @@ final_projections <- projection_table_combined[
                                                   , "Tampa Bay Buccaneers" = "TB"
                                                   , "New York Giants" = "NYG"
                                                   , "Houston Texans" = "HOU"), trimws(RIGHT(PLAYER.V1, 3)))
-        , POINTS = POINTS.MISC)
+        , POINTS = as.numeric(POINTS.MISC))
   
-  ]
+  ][, TEAM := as.character(TEAM1)][!POSITION == "K", ][TEAM %in% c("BUF", "NE", "PIT", "PHI", 
+                                                                   "WAS", "BAL", "IND", "SF", 
+                                                                   "KC", "DEN", "LAC", "CIN", 
+                                                                   "TEN", "LV", "NYJ", "ARI",
+                                                                   "ATL", "GB", "DET", "LAR",
+                                                                   "MIA", "CHI", "DAL", "SEA",
+                                                                   "CAR", "CLE", "MIN", "NO",
+                                                                   "JAC", "TB", "NYG", "HOU")]
+
+saveRDS(final_projections, paste0("Data/projections_week", week_number, "_", Sys.Date(), ".RDS"))
