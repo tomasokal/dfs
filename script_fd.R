1f1a1a5444c9a47f0f094c4470a0d88bf6378805
Tomas Okal <tomas.okal@gmail.com>
2020-09-30 18:12:36 -0500

    Adding scripts to automatically run through and optimize for main daily sites. Will provide CSV files for default optimization.

---
+++ script_fd.R
@@ -0,0 +1,36 @@
+# Necessary libraries
+library(data.table)
+library(lpSolve)
+
+# Setting the non-slate games
+NON_SLATE <- c("NYJ", "DEN", "PHI", "SF", "ATL", "GB")
+
+  # Needs to be automated.
+
+# Optimize
+player_pool <- merge_full[!TEAM %in% NON_SLATE][!is.na(SALARY_FD)]
+obj_points <- player_pool[, .(POINTS = POINTS_FD)]
+position_dt <- player_pool[, j = .(ppQB = ifelse(POSITION == "QB", 1, 0),
+                                   ppRB = ifelse(POSITION == "RB", 1, 0),
+                                   ppWR = ifelse(POSITION == "WR", 1, 0),
+                                   ppTE = ifelse(POSITION == "TE", 1, 0),
+                                   ppDST = ifelse(POSITION == "DST", 1, 0),
+                                   ppFlex = ifelse(POSITION %in% c("RB", "WR", "TE"), 1, 0))]
+
+con_players <- t(cbind(SALARY = player_pool[, SALARY_FD], position_dt))
+colnames(con_players) <- player_pool$PLAYER
+
+f.dir <- rep(0, nrow(con_players))
+f.rhs <- rep(0, nrow(con_players))
+
+f.dir[1] <- "<="
+f.rhs[1] <- 60000
+
+f.dir[2:nrow(con_players)] <- c("=", ">=", ">=", ">=", "=", "=")
+f.rhs[2:nrow(con_players)] <- c(1, 2, 3, 1, 1, 7)
+
+opt <- lp("max", obj_points, con_players, f.dir, f.rhs, all.bin = TRUE)
+picks_fd <- player_pool[which(opt$solution == 1), ][, .(PLAYER, POSITION, TEAM, POINTS = POINTS_FD, SALARY = SALARY_FD)]
+
+# Export data 
+data.table::fwrite(picks_fd, "Output/picks_fd.csv")
