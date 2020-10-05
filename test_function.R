0374c3abfc70a36e42a67441490d2f62bcc0184e
Tomas Okal <tomas.okal@gmail.com>
2020-10-05 15:35:36 -0500

    test_function

---
+++ test_function.R
@@ -0,0 +1,7 @@
+library(data.table)
+library(lpSolve)
+
+df_full <- data.table::fread("Output/salaries_projections_scraped_script.csv")[!TEAM %in% NON_SLATE][!is.na(SALARY_YH)]
+df_include <- df_full[PLAYER %in% c("Kyler Murray", "Lamar Jackson", "Curtis Samuel")]
+df_exclude <- df_full[PLAYER %in% c("asdfasdfa")]
+optimize_dk(df_full, df_include, df_exclude)  
