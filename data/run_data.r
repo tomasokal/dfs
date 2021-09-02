4e53c738af691cfac5fcd45d4b5cedbe8b58b8fa
Tomas Okal <tomas.okal@gmail.com>
2021-09-02 00:18:53 -0500

    Task: Scrape Schedule

--- data/run_data.r
+++ data/run_data.r
@@ -5,4 +5,7 @@ library(xml2)
 
 # Build Projections
 source("projections/1_projections.R")
-source("projections/2_projections.R")
+source("projections/2_projections_defense.R")
+
+# Pull Schedule
+source("schedule/1_nfl_schedule.R")
--- data/schedule/1_nfl_schedule.R
+++ data/schedule/1_nfl_schedule.R
@@ -1,7 +1,7 @@
 function_scrape_schedule <- function( x ) {
   
   # Source of data
-  url <- paste0("https://nflgamedata.com/schedule.php?season=2021&amp;week=", x)
+  url <- paste0("https://nflgamedata.com/schedule.php?season=2021&week=", x)
   page <- rvest::html_table(xml2::read_html(url)
                             , header = TRUE
                             , fill = TRUE)
@@ -9,28 +9,43 @@ function_scrape_schedule <- function( x ) {
   # Parse data
   scrape_schedule <- data.table::data.table(page[[6]])
   
-  # Remove headers
-  scrape_schedule <- scrape_schedule[-1, ]
-  
   # Change column names
   colnames(scrape_schedule) <- paste0("v", 1:ncol(scrape_schedule))
   
   # Change NA to 0
-  scrape_schedule$v12[is.na(scrape_schedule$v12)] <- 0
-  scrape_schedule$v14[is.na(scrape_schedule$v14)] <- 0
+  scrape_schedule$v12[is.na(scrape_schedule$v12) | scrape_schedule$v12 == "PK"] <- 0
+  scrape_schedule$v14[is.na(scrape_schedule$v14) | scrape_schedule$v14 == "PK"] <- 0
+  
+  # Force numeric
+  scrape_schedule <- scrape_schedule[, v14 := as.numeric(as.character(v14))]
   
   # Select features
-  scrape_schedule <- scrape_schedule[, .(DAY = v4
-                                         , DATE = v5
+  scrape_schedule <- scrape_schedule[, ODDS_HOME := rowSums(.SD, na.rm = TRUE), .SDcols = c(12, 14)]
+  scrape_schedule <- scrape_schedule[, .(WEEK = x
+                                         , DAY = v4
+                                         , DATE = as.Date(v5, format = "%m/%d")
                                          , TIME = v6
                                          , TEAM_AWAY = v9
                                          , TEAM_HOME = v17
                                          , OVERUNDER = v20
-                                         , ODDS_HOME = v14 - v12
+                                         , ODDS_HOME
                                          )
                                      ]
   
+  # Remove teams with byes
+  scrape_schedule[scrape_schedule==''|scrape_schedule==' '] <- NA
+  scrape_schedule <- scrape_schedule[!is.na(DAY), ]
+  
   return(scrape_schedule)
   
-  
 }
+
+mydate <- Sys.Date()
+
+## TODO 
+## Make it to where this script will only pull weeks that have not yet happened.
+## Games might get rearranged, but if the week has already happened, do not need to refer to it.
+
+scrape_schedule <- data.table::rbindlist(lapply(seq(1:18), function_scrape_schedule))
+
+
