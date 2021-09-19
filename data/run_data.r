c8f70203be2ece36c908429c6f8217e0e81216f4
Tomas Okal <tomas.okal@gmail.com>
2021-09-18 21:04:48 -0500

    Update to run_data

--- data/run_data.r
+++ data/run_data.r
@@ -6,6 +6,7 @@ library(xml2)
 # Build Projections
 source("projections/1_projections.R")
 source("projections/2_projections_defense.R")
+source("projections/3_projections_save.R")
 
 # Pull Schedule
 source("schedule/1_nfl_schedule.R")
