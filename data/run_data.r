4cd607f7d8ef4a404316fa53d9b69f5f45603514
Tomas Okal <tomas.okal@gmail.com>
2021-09-18 21:21:29 -0500

    Add in file that will constantly be updated so Github actions always has something to commit.

--- data/run_data.r
+++ data/run_data.r
@@ -3,6 +3,9 @@ library(data.table)
 library(rvest)
 library(xml2)
 
+# Write a sys.time object to have a new file to commit every time.
+data.table::fwrite(data.table::as.data.table(Sys.time()), 'data/_export/run_time.csv')
+
 # Build Projections
 source("data/projections/1_projections.R")
 source("data/projections/2_projections_defense.R")
