f9dcd223d92a4dd6ca3da043c51a671f99c949ed
Tomas Okal <tomas.okal@gmail.com>
2022-07-30 17:07:46 -0500

    Test update.

--- dependencies.R
+++ dependencies.R
@@ -1,6 +1,6 @@
 # This file allows packrat (used by rsconnect during deployment) to pick up dependencies.
 library(data.table)
-library(highcharter)
+library(echarts4r)
 library(htmlwidgets)
 library(reactable)
 library(rhino)
