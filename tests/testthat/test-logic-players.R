27883aabe497d68759f2b6e0538129aa65b1c623
Tomas Okal <tomas.okal@gmail.com>
2022-07-31 23:09:25 -0500

    Fix failing test.

--- tests/testthat/test-logic-players.R
+++ tests/testthat/test-logic-players.R
@@ -1,5 +1,4 @@
 box::use(
-  shiny[testServer],
   testthat[...],
 )
 box::use(
@@ -10,7 +9,7 @@ test_that("Function get_data() returns expected features", {
 
   test_df <- data.frame(
     PLAYER = 1,
-    TEAM = 1, 
+    TEAM = 1,
     POSITION = 1,
     POINTS_DK = 1,
     SALARY_DK = 1
@@ -19,6 +18,5 @@ test_that("Function get_data() returns expected features", {
   app_df <- get_data()
 
   expect_equal(colnames(test_df), colnames(app_df))
-  expect_equal(ncols(test_df), ncols(app_df))
 
 })
